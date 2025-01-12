098f6df88ab6f0aaabb7cf25aa48c126de6078a4
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-01-12 17:51:03 +0530

    Time: 27 ms (5.96%) | Memory: 43.1 MB (83.02%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        Map<Character, Integer> map = new HashMap();
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        for(int count : map.values()){
+            if(count!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
