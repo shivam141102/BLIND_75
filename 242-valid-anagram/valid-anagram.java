70590eff19b3f400c57d477c9dbbba06615628e4
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-01-24 18:59:35 +0530

    Time: 1 ms (99.82%) | Memory: 43.5 MB (64.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,56 @@
+// import java.util.Arrays;
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         char[] sChars = s.toCharArray();
+//         char[] tChars = t.toCharArray();
+        
+//         Arrays.sort(sChars);
+//         Arrays.sort(tChars);
+        
+//         return Arrays.equals(sChars, tChars);
+//     }
+// }
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()) return false;
+//         Map<Character, Integer> map = new HashMap();
+//         for(int i = 0; i < s.length(); i++){
+//             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+//             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+//         }
+//         for(int count : map.values()){
+//             if(count!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        Map<Character, Integer> map = new HashMap();
-        for(int i = 0; i < s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        int[] count = new int[26];
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
         }
-        for(int count : map.values()){
-            if(count!=0){
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
                 return false;
             }
         }
+        
         return true;
     }
 }
