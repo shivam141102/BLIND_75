5678ec33c9a8900d2458266aab55c1717a182325
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-01-24 18:42:56 +0530

    Time: 16 ms (20.62%) | Memory: 48.9 MB (32.81%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,46 @@
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         // Create a frequency map
+//         Map<Integer, Integer> frequencyMap = new HashMap<>();
+//         for (int num : nums) {
+//             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+//         }
+
+//         // Convert the frequency map to a list and sort by frequency in descending order
+//         List<Map.Entry<Integer, Integer>> sortedEntries = new ArrayList<>(frequencyMap.entrySet());
+//         sortedEntries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+//         // Extract the top K keys into the result array
+//         int[] result = new int[k];
+//         for (int i = 0; i < k; i++) {
+//             result[i] = sortedEntries.get(i).getKey();
+//         }
+
+//         return result;
+//     }
+// }
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // Create a frequency map
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-
-        int arr[] = new int[k];
-        int i = 0;
-        while(k!=0){
-            arr[i] = list.get(i).getKey();
-            i++;
-            k--;
+        PriorityQueue <Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(Map.Entry.comparingByValue());
+        
+        for(Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()){
+            minHeap.offer(entry);
+            if(minHeap.size() > k){
+                minHeap.poll();
+            }
         }
-        return arr;
+        int[] result = new int [k];
+        int index = 0;
+        while(!minHeap.isEmpty()){
+            result[index++] = minHeap.poll().getKey();
+        }
+        return result;
     }
 }
