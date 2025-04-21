4f2df576c4165430e2f597702a71867bdf09ba0f
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-04-21 18:04:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (38.79%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -33,22 +33,58 @@
 // }
 
 // TABULATION
+// class Solution {
+//     public int Rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+
+//         dp[0] = nums[0];
+//         for(int i = 1; i < n; i++){
+//             int pick = nums[i];
+//             if(i > 1){
+//                 pick += dp[i - 2];
+//             }
+//             int notPick = 0 + dp[i - 1];
+//             dp[i] = Math.max(pick, notPick);
+//         }
+//         return dp[n-1];
+//     }
+
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         if(n == 1) return nums[0];
+//         int[] nums1 = new int[n - 1];
+//         int[] nums2 = new int[n - 1];
+
+//         for(int i = 0; i < n - 1; i++){
+//             nums1[i] = nums[i];
+//         }
+//         for(int i = 1; i < n; i++){
+//             nums2[i - 1] = nums[i];
+//         }
+//         return Math.max(Rob(nums1), Rob(nums2));
+//     }
+// }
+
+// SPACE OPTIMIZATION
 class Solution {
     public int Rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+        int prev = nums[0];
+        int prev2 = 0;
 
-        dp[0] = nums[0];
         for(int i = 1; i < n; i++){
             int pick = nums[i];
             if(i > 1){
-                pick += dp[i - 2];
+                pick += prev2;
             }
-            int notPick = 0 + dp[i - 1];
-            dp[i] = Math.max(pick, notPick);
+            int notPick = 0 + prev;
+            int curr = Math.max(pick, notPick);
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n-1];
+        return prev;
     }
 
     public int rob(int[] nums) {
@@ -66,24 +102,3 @@ class Solution {
         return Math.max(Rob(nums1), Rob(nums2));
     }
 }
-
-// SPACE OPTIMIZATION
-// class Solution {
-//     public int rob(int[] nums) {
-//         int n = nums.length;
-//         int prev = nums[0];
-//         int prev2 = 0;
-
-//         for(int i = 1; i < n; i++){
-//             int pick = nums[i];
-//             if(i > 1){
-//                 pick += prev2;
-//             }
-//             int notPick = 0 + prev;
-//             int curr = Math.max(pick, notPick);
-//             prev2 = prev;
-//             prev = curr;
-//         }
-//         return prev;
-//     }
-// }
