235779655225c088f7fa9862a0b1e37e92328fb3
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-04-21 15:13:29 +0530

    Time: 112 ms (7.31%) | Memory: 44.9 MB (36.53%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -35,51 +35,32 @@
 
 
 class Solution {
-    // public int f(int ind, int amount, int n, int[] coins, int[][] dp){
-    //     if(ind == 0){
-    //         if(amount % coins[0] == 0){
-    //             return amount/coins[0];
-    //         }
-    //         return (int) Math.pow(10, 9);
-    //     }
-    //     if(dp[ind][amount] != -1) return dp[ind][amount];
-
-    //     int notTake = 0 + f(ind - 1, amount, n, coins, dp);
-    //     int take = (int) Math.pow(10, 9);
-    //     if(coins[ind] <= amount){
-    //         take = 1 + f(ind, amount - coins[ind], n, coins, dp);
-    //     }
-    //     return dp[ind][amount] = Math.min(notTake, take);
-    // }
-
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
+        int[] prev = new int[amount + 1];
+        int[] curr = new int[amount + 1];
 
         for(int target = 0; target <= amount; target++){
             if(target % coins[0] == 0){
-                dp[0][target] = target/coins[0];
+                prev[target] = target/coins[0];
             }
             else{
-                dp[0][target] = (int) Math.pow(10, 9);
+                prev[target] = (int) Math.pow(10, 9);
             }
         }
 
         for(int ind = 1; ind < n; ind++){
             for(int target = 0; target <= amount; target++){
-                int notTake = 0 + dp[ind - 1][target];
+                int notTake = 0 + prev[target];
                 int take = (int) Math.pow(10, 9);
                 if(coins[ind] <= target){
-                    take = 1 + dp[ind][target - coins[ind]];
+                    take = 1 + curr[target - coins[ind]];
                 }
-                dp[ind][target] = Math.min(notTake, take);
+                curr[target] = Math.min(notTake, take);
             }
+            prev = curr.clone();
         }
-        int ans = dp[n - 1][amount];
+        int ans = prev[amount];
 
         if (ans >= (int) Math.pow(10, 9))
             return -1;
