2951e5467ec2fdcf600d7e1e3221ade587aaad7f
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-04-21 16:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (38.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,34 @@
-class Solution {
-    public int f(int n, int[] dp){
-        if(n == 0 || n == 1) return 1;
-        if(dp[n] != -1) return dp[n];
+// class Solution {
+//     public int f(int n, int[] dp){
+//         if(n == 0 || n == 1) return 1;
+//         if(dp[n] != -1) return dp[n];
 
-        return dp[n] = f(n - 1, dp) + f(n - 2, dp);
-    }
+//         return dp[n] = f(n - 1, dp) + f(n - 2, dp);
+//     }
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n + 1];
+//         for(int i = 0; i <= n; i++){
+//             dp[i] = -1;
+//         }
+//         return f(n, dp);
+//     }
+// }
+
+
+class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        for(int i = 0; i <= n; i++){
-            dp[i] = -1;
+        // for(int i = 0; i <= n; i++){
+        //     dp[i] = -1;
+        // }
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int ind = 2; ind <=n ; ind++){
+            dp[ind] = dp[ind - 1] + dp[ind - 2];
         }
-        return f(n, dp);
+
+        return dp[n];
     }
 }
