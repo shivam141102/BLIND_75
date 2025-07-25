53c714bad2e1f987acf0c38767b9453783232572
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-07-25 17:00:14 +0530

    Time: 5 ms (9.88%) | Memory: 47.4 MB (74.06%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        List<List<Integer>> adj = getAdjacencyList(isConnected);
+        int province = 0;
+        boolean[] vis = new boolean[isConnected.length + 1];
+        for(int i = 0; i < isConnected.length; i++){
+            if(vis[i] == false){
+                bfs(i, adj, vis);
+                province++;
+            }
+        }
+        return province;
+    }
+
+    public List<List<Integer>> getAdjacencyList(int[][] isConnected){
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int[] edge : isConnected){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < isConnected.length; i++){
+            for(int j = 0; j < isConnected[i].length; j++){
+                if(i == j) continue;
+                if(isConnected[i][j] == 1){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+        return adj;
+    }
+
+    public void bfs(int start, List<List<Integer>> adj, boolean[] vis){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        vis[start] = true;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int neighbour: adj.get(node)){
+                if(vis[neighbour] == false){
+                    vis[neighbour] = true;
+                    q.offer(neighbour);
+                }
+            }
+        }
+    }
+}
