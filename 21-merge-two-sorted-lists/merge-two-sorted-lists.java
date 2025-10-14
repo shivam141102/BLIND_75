3853fab387c7d3cdf76b30d3d523fbe40514cbd9
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-10-14 09:10:14 -0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (47.38%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -8,30 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode dummy = new ListNode(-1);
+//         ListNode ans = dummy;
+
+//         while(list1 != null && list2 != null){
+//             if(list1.val <= list2.val){
+//                 dummy.next = list1;
+//                 list1 = list1.next;
+//             }
+//             else{
+//                 dummy.next = list2;
+//                 list2 = list2.next;
+//             }
+//             dummy = dummy.next;
+//         }
+
+//         if(list1 == null && list2 != null){
+//             dummy.next = list2;
+//         }
+//         if(list2 == null && list1 != null){
+//             dummy.next = list1;
+//         }
+//         return ans.next;
+//     }
+// }
+
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode ans = dummy;
-
-        while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
-                dummy.next = list1;
-                list1 = list1.next;
-            }
-            else{
-                dummy.next = list2;
-                list2 = list2.next;
-            }
-            dummy = dummy.next;
+        if(list1 == null || list2 == null){
+            return list1 == null ? list2 : list1;
         }
 
-        if(list1 == null && list2 != null){
-            dummy.next = list2;
+        if(list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }
-        if(list2 == null && list1 != null){
-            dummy.next = list1;
+        else{
+         
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-
-        return ans.next;
     }
 }
