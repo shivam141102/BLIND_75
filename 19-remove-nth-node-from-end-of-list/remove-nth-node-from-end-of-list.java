f2f2e6352c17ee0dfc2db4b25e360c9b5971a9b3
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-10-27 08:22:37 -0500

    Time: 0 ms (100.00%) | Memory: 41.8 MB (47.01%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,22 +10,39 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode curr = head;
-        int cnt = 0;
-        while(curr != null){
-            cnt++;
-            curr = curr.next;
-        }
+    //     ListNode curr = head;
+    //     int cnt = 0;
+    //     while(curr != null){
+    //         cnt++;
+    //         curr = curr.next;
+    //     }
 
-        curr = head;
-        int res = cnt - n;
+    //     curr = head;
+    //     int res = cnt - n;
 
-        if(res == 0) return head.next;
-        while(res > 1 && curr != null){
-            curr = curr.next;
-            res--;
-        }
-        curr.next = curr.next.next;
-        return head;
+    //     if(res == 0) return head.next;
+    //     while(res > 1 && curr != null){
+    //         curr = curr.next;
+    //         res--;
+    //     }
+    //     curr.next = curr.next.next;
+    //     return head;
+
+    ListNode front = head, back = head;
+    int second = n;
+    while(second != 0){
+        second--;
+        front = front.next;
+    }
+
+    //Case when n == cnt
+    if (front == null) return head.next;
+
+    while(front.next != null){
+        front = front.next;
+        back = back.next;
+    }
+    back.next = back.next.next;
+    return head;
     }
 }
