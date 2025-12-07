3a80cdf7467af928a502b2d473f400f8772201d4
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-12-06 18:37:26 -0600

    Time: 32 ms (18.24%) | Memory: 46.9 MB (19.11%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,69 +1,28 @@
-// class Solution {
-//     public int f(int ind, int amount, int n, int[] coins, int[][] dp){
-//         if(ind == 0){
-//             if(amount % coins[0] == 0){
-//                 return amount/coins[0];
-//             }
-//             return (int) Math.pow(10, 9);
-//         }
-//         if(dp[ind][amount] != -1) return dp[ind][amount];
-
-//         int notTake = 0 + f(ind - 1, amount, n, coins, dp);
-//         int take = (int) Math.pow(10, 9);
-//         if(coins[ind] <= amount){
-//             take = 1 + f(ind, amount - coins[ind], n, coins, dp);
-//         }
-//         return dp[ind][amount] = Math.min(notTake, take);
-//     }
-
-//     public int coinChange(int[] coins, int amount) {
-//         int n = coins.length;
-//         int[][] dp = new int[n][amount + 1];
-
-//         for(int[] row : dp){
-//             Arrays.fill(row, -1);
-//         }
-
-//         int ans = f(coins.length - 1, amount, coins.length, coins, dp);
-//         if(ans >= (int) Math.pow(10, 9)){
-//             return -1;
-//         }
-//         return ans;
-//     }
-// }
-
-
-
 class Solution {
+    public int f(int ind, int amount, int[] coins, int[][] dp){
+        if(ind == 0 ){
+            if(amount % coins[0] == 0) return amount/coins[0];
+            return (int)1e9;
+        }
+
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+        
+        int notTake = 0 + f(ind - 1, amount, coins, dp);
+        int take = Integer.MAX_VALUE;
+        if(coins[ind] <= amount){
+            take = 1 + f(ind, amount - coins[ind], coins, dp);
+        }
+        return dp[ind][amount] = Math.min(take, notTake);
+    }
+
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[] prev = new int[amount + 1];
-        int[] curr = new int[amount + 1];
-
-        for(int target = 0; target <= amount; target++){
-            if(target % coins[0] == 0){
-                prev[target] = target/coins[0];
-            }
-            else{
-                prev[target] = (int) Math.pow(10, 9);
+        int[][] dp = new int[coins.length][amount + 1];
+        for(int i = 0; i < coins.length; i++){
+            for(int j = 0; j <= amount; j++){
+                dp[i][j] = -1;
             }
         }
-
-        for(int ind = 1; ind < n; ind++){
-            for(int target = 0; target <= amount; target++){
-                int notTake = 0 + prev[target];
-                int take = (int) Math.pow(10, 9);
-                if(coins[ind] <= target){
-                    take = 1 + curr[target - coins[ind]];
-                }
-                curr[target] = Math.min(notTake, take);
-            }
-            prev = curr.clone();
-        }
-        int ans = prev[amount];
-
-        if (ans >= (int) Math.pow(10, 9))
-            return -1;
-        return ans;
+        int ans = f(coins.length - 1, amount, coins, dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 }
