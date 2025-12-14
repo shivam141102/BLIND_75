2350fcb709e346bd1a82b142dd42d596426ef077
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-12-14 17:08:18 -0600

    Time: 19 ms (66.63%) | Memory: 54.4 MB (29.16%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int helper(int i, int j, String s, String t, int[][] dp){
+        if(j < 0) return 1;
+        if(i < 0) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s.charAt(i) == t.charAt(j)){
+            return dp[i][j] = helper(i - 1, j - 1, s, t, dp) + helper(i - 1, j, s, t, dp);
+        }
+        else{
+            return dp[i][j] = helper(i - 1, j, s, t, dp);
+        }
+    }
+
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        
+        int index1 = s.length();
+        int index2 = t.length();
+
+        for (int i = 0 ;i < index1; i++) {
+            for (int j = 0; j < index2; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return helper(index1 - 1, index2 - 1, s, t, dp);
+    }
+}
