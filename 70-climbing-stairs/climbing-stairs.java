a5ba8f309c13dade87dbac50415ac17b14b52e2a
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-12-24 20:23:32 -0600

    Time: 0 ms (100.00%) | Memory: 42.3 MB (10.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,52 +1,14 @@
-// Memoization
-//class Solution {
-//     public int f(int n, int[] dp){
-//         if(n == 0 || n == 1) return 1;
-//         if(dp[n] != -1) return dp[n];
-
-//         return dp[n] = f(n - 1, dp) + f(n - 2, dp);
-//     }
-//     public int climbStairs(int n) {
-//         int[] dp = new int[n + 1];
-//         for(int i = 0; i <= n; i++){
-//             dp[i] = -1;
-//         }
-//         return f(n, dp);
-//     }
-// }
-
-// Tabulation
-// class Solution {
-//     public int climbStairs(int n) {
-//         int[] dp = new int[n + 1];
-//         // for(int i = 0; i <= n; i++){
-//         //     dp[i] = -1;
-//         // }
-
-//         dp[0] = 1;
-//         dp[1] = 1;
-
-//         for(int ind = 2; ind <=n ; ind++){
-//             dp[ind] = dp[ind - 1] + dp[ind - 2];
-//         }
-
-//         return dp[n];
-//     }
-// }
-
-
-// Space Optimization
 class Solution {
+    public int helper(int i, int[] dp){
+        if(i < 3) return i;
+
+        if(dp[i] != 0) return dp[i];
+
+        return dp[i] = helper(i - 1, dp) + helper(i - 2, dp);
+    }
+
     public int climbStairs(int n) {
-        int prev1 = 1;
-        int prev2 = 1;
-
-        for(int ind = 2; ind <=n ; ind++){
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        return prev1;
+        int[] dp = new int[n + 1];
+        return helper(n, dp);
     }
 }
