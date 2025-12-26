e400388c6c7bd5ea9f2935365a281aa7514ab175
Shivam Mishra <81294217+shivam141102@users.noreply.github.com>
2025-12-25 18:53:05 -0600

    Time: 5 ms (25.06%) | Memory: 50.6 MB (7.29%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,20 +1,44 @@
+// class Solution {
+//     public int helper(int i, int j, int[][] grid, int[][] dp){
+//         if(i == 0 && j == 0) return grid[0][0];
+//         if(i < 0 || j < 0) return Integer.MAX_VALUE;
+
+//         if(dp[i][j] != 0) return dp[i][j];
+
+//         int up = helper(i - 1, j, grid, dp);
+//         int left = helper(i, j - 1, grid, dp);
+
+//         return dp[i][j] = grid[i][j] + Math.min(up, left);
+//     }
+//     public int minPathSum(int[][] grid) {
+//         int row = grid.length;
+//         int col = grid[0].length;
+
+//         int[][] dp = new int[row][col];
+//         return helper(row - 1, col - 1, grid, dp);
+//     }
+// }
+
+
 class Solution {
-    public int helper(int i, int j, int[][] grid, int[][] dp){
-        if(i == 0 && j == 0) return grid[0][0];
-        if(i < 0 || j < 0) return Integer.MAX_VALUE;
-
-        if(dp[i][j] != 0) return dp[i][j];
-
-        int up = helper(i - 1, j, grid, dp);
-        int left = helper(i, j - 1, grid, dp);
-
-        return dp[i][j] = grid[i][j] + Math.min(up, left);
-    }
     public int minPathSum(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
 
         int[][] dp = new int[row][col];
-        return helper(row - 1, col - 1, grid, dp);
+        dp[0][0] = grid[0][0];
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(i == 0 && j == 0) continue;
+                
+                int down = Integer.MAX_VALUE, right = Integer.MAX_VALUE;                
+                if(i - 1 >= 0) down = dp[i - 1][j];
+                if(j - 1 >= 0) right = dp[i][j - 1];
+
+                dp[i][j] = grid[i][j] + Math.min(down, right);
+            }
+        }
+        return dp[row - 1][col - 1];
     }
 }
